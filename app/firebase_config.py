d8894966a3e8101fab40ce17386edd900965d56a
Kevin Nderi Nderitu <kelvinnderitu40@gmail.com>
2026-01-12 15:46:39 +0300

    Ready for Render deployment with Firebase env

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -1,14 +1,13 @@
+import os
 import pyrebase
+import json
 
-firebaseConfig = {
-    "apiKey": "AIzaSyDWjuSCRf5bFIYwCRY_jaCOxSrlu-JG3f4",
-    "authDomain": "bloodbank-management-syetem.firebaseapp.com",
-    "databaseURL": "https://bloodbank-management-syetem-default-rtdb.asia-southeast1.firebasedatabase.app",
-    "projectId": "bloodbank-management-syetem",
-    "storageBucket": "bloodbank-management-syetem.appspot.com",
-    "messagingSenderId": "685674388951",
-    "appId": "1:685674388951:web:16c6ee2d60eecaf8d5ff92"
-}
+# Read Firebase config from environment variable
+firebase_config_json = os.environ.get("FIREBASE_CONFIG")
+if firebase_config_json is None:
+    raise Exception("FIREBASE_CONFIG environment variable not set!")
+
+firebaseConfig = json.loads(firebase_config_json)
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
